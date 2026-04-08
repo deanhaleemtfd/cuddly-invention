2f739efb203a1c9fcc734abb77d15c0e662177c0
dean <dhaleem@thefarmersdog.com>
2026-04-08 13:03:41 -0400

    fly, you fools

---
+++ product-combinations/core.js
@@ -0,0 +1,194 @@
+[
+  {
+    dailyKcal: 133,
+    kcalPerPack: 529,
+    dailyPortion: 0.25,
+    packSize: '05',
+    key: 'core:529:0.25',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 152,
+    kcalPerPack: 608,
+    dailyPortion: 0.25,
+    packSize: '06',
+    key: 'core:608:0.25',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 175,
+    kcalPerPack: 699,
+    dailyPortion: 0.25,
+    packSize: '07',
+    key: 'core:699:0.25',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 201,
+    kcalPerPack: 804,
+    dailyPortion: 0.25,
+    packSize: '08',
+    key: 'core:804:0.25',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 231,
+    kcalPerPack: 924,
+    dailyPortion: 0.25,
+    packSize: '09',
+    key: 'core:924:0.25',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 265,
+    kcalPerPack: 529,
+    dailyPortion: 0.5,
+    packSize: '05',
+    key: 'core:529:0.5',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 304,
+    kcalPerPack: 608,
+    dailyPortion: 0.5,
+    packSize: '06',
+    key: 'core:608:0.5',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 350,
+    kcalPerPack: 699,
+    dailyPortion: 0.5,
+    packSize: '07',
+    key: 'core:699:0.5',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 402,
+    kcalPerPack: 804,
+    dailyPortion: 0.5,
+    packSize: '08',
+    key: 'core:804:0.5',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 462,
+    kcalPerPack: 924,
+    dailyPortion: 0.5,
+    packSize: '09',
+    key: 'core:924:0.5',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 532,
+    kcalPerPack: 1063,
+    dailyPortion: 0.5,
+    packSize: '10',
+    key: 'core:1063:0.5',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 608,
+    kcalPerPack: 608,
+    dailyPortion: 1,
+    packSize: '06',
+    key: 'core:608:1',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 699,
+    kcalPerPack: 699,
+    dailyPortion: 1,
+    packSize: '07',
+    key: 'core:699:1',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 804,
+    kcalPerPack: 804,
+    dailyPortion: 1,
+    packSize: '08',
+    key: 'core:804:1',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 924,
+    kcalPerPack: 924,
+    dailyPortion: 1,
+    packSize: '09',
+    key: 'core:924:1',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 1063,
+    kcalPerPack: 1063,
+    dailyPortion: 1,
+    packSize: '10',
+    key: 'core:1063:1',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 1216,
+    kcalPerPack: 608,
+    dailyPortion: 2,
+    packSize: '06',
+    key: 'core:608:2',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 1398,
+    kcalPerPack: 699,
+    dailyPortion: 2,
+    packSize: '07',
+    key: 'core:699:2',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 1608,
+    kcalPerPack: 804,
+    dailyPortion: 2,
+    packSize: '08',
+    key: 'core:804:2',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 1848,
+    kcalPerPack: 924,
+    dailyPortion: 2,
+    packSize: '09',
+    key: 'core:924:2',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 2126,
+    kcalPerPack: 1063,
+    dailyPortion: 2,
+    packSize: '10',
+    key: 'core:1063:2',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 2412,
+    kcalPerPack: 804,
+    dailyPortion: 3,
+    packSize: '08',
+    key: 'core:804:3',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 2772,
+    kcalPerPack: 924,
+    dailyPortion: 3,
+    packSize: '09',
+    key: 'core:924:3',
+    productLine: 'core'
+  },
+  {
+    dailyKcal: 3189,
+    kcalPerPack: 1063,
+    dailyPortion: 3,
+    packSize: '10',
+    key: 'core:1063:3',
+    productLine: 'core'
+  }
+]
---
+++ product-combinations/low-fat.js
@@ -0,0 +1,194 @@
+[
+    {
+        dailyKcal: 133,
+        kcalPerPack: 529,
+        dailyPortion: 0.25,
+        packSize: '05',
+        key: 'low-fat: 529: 0.25',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 152,
+        kcalPerPack: 608,
+        dailyPortion: 0.25,
+        packSize: '06',
+        key: 'low-fat: 608: 0.25',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 175,
+        kcalPerPack: 699,
+        dailyPortion: 0.25,
+        packSize: '07',
+        key: 'low-fat: 699: 0.25',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 201,
+        kcalPerPack: 804,
+        dailyPortion: 0.25,
+        packSize: '08',
+        key: 'low-fat: 804: 0.25',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 231,
+        kcalPerPack: 462,
+        dailyPortion: 0.5,
+        packSize: '04',
+        key: 'low-fat: 462: 0.5',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 265,
+        kcalPerPack: 529,
+        dailyPortion: 0.5,
+        packSize: '05',
+        key: 'low-fat: 529: 0.5',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 304,
+        kcalPerPack: 608,
+        dailyPortion: 0.5,
+        packSize: '06',
+        key: 'low-fat: 608: 0.5',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 350,
+        kcalPerPack: 699,
+        dailyPortion: 0.5,
+        packSize: '07',
+        key: 'low-fat: 699: 0.5',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 402,
+        kcalPerPack: 804,
+        dailyPortion: 0.5,
+        packSize: '08',
+        key: 'low-fat: 804: 0.5',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 462,
+        kcalPerPack: 462,
+        dailyPortion: 1,
+        packSize: '04',
+        key: 'low-fat: 462: 1',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 529,
+        kcalPerPack: 529,
+        dailyPortion: 1,
+        packSize: '05',
+        key: 'low-fat: 529: 1',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 608,
+        kcalPerPack: 608,
+        dailyPortion: 1,
+        packSize: '06',
+        key: 'low-fat: 608: 1',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 699,
+        kcalPerPack: 699,
+        dailyPortion: 1,
+        packSize: '07',
+        key: 'low-fat: 699: 1',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 804,
+        kcalPerPack: 804,
+        dailyPortion: 1,
+        packSize: '08',
+        key: 'low-fat: 804: 1',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 924,
+        kcalPerPack: 462,
+        dailyPortion: 2,
+        packSize: '04',
+        key: 'low-fat: 462: 2',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 1058,
+        kcalPerPack: 529,
+        dailyPortion: 2,
+        packSize: '05',
+        key: 'low-fat: 529: 2',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 1216,
+        kcalPerPack: 608,
+        dailyPortion: 2,
+        packSize: '06',
+        key: 'low-fat: 608: 2',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 1398,
+        kcalPerPack: 699,
+        dailyPortion: 2,
+        packSize: '07',
+        key: 'low-fat: 699: 2',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 1608,
+        kcalPerPack: 804,
+        dailyPortion: 2,
+        packSize: '08',
+        key: 'low-fat: 804: 2',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 1824,
+        kcalPerPack: 608,
+        dailyPortion: 3,
+        packSize: '06',
+        key: 'low-fat: 608: 3',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 2097,
+        kcalPerPack: 699,
+        dailyPortion: 3,
+        packSize: '07',
+        key: 'low-fat: 699: 3',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 2412,
+        kcalPerPack: 804,
+        dailyPortion: 3,
+        packSize: '08',
+        key: 'low-fat: 804: 3',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 2796,
+        kcalPerPack: 699,
+        dailyPortion: 4,
+        packSize: '07',
+        key: 'low-fat: 699: 4',
+        productLine: 'low-fat'
+    },
+    {
+        dailyKcal: 3216,
+        kcalPerPack: 804,
+        dailyPortion: 4,
+        packSize: '08',
+        key: 'low-fat: 804: 4',
+        productLine: 'low-fat'
+    }
+]
